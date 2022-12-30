f0c858b2b5f4e6d40c0e32845e666795e18a47ef
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-12-30 08:52:16 +0300

    no message

--- app/src/main/java/com/example/unite4_kararvedongu/Uyg6Activity.java
+++ app/src/main/java/com/example/unite4_kararvedongu/Uyg6Activity.java
@@ -1,6 +1,7 @@
 package com.example.unite4_kararvedongu;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,5 +20,52 @@ public class Uyg6Activity extends AppCompatActivity {
         txteditnmbr = findViewById(R.id.txteditnmbr);
         btn = findViewById(R.id.btnuyg6);
         txtview = findViewById(R.id.txtview);
+    }
+    public void onayla(View view){
+        int sayiay = Integer.parseInt(txteditnmbr.getText().toString());
+        String ay="";
+        txteditnmbr.setText("");
+        switch (sayiay){
+            case 1:
+                ay="OCAK";
+                break;
+            case 2:
+                ay="OCAK";
+                break;
+            case 3:
+                ay="OCAK";
+                break;
+            case 4:
+                ay="OCAK";
+                break;
+            case 5:
+                ay="OCAK";
+                break;
+            case 6:
+                ay="OCAK";
+                break;
+            case 7:
+                ay="OCAK";
+                break;
+            case 8:
+                ay="OCAK";
+                break;
+            case 9:
+                ay="OCAK";
+                break;
+            case 10:
+                ay="OCAK";
+                break;
+            case 11:
+                ay="OCAK";
+                break;
+            case 12:
+                ay="OCAK";
+                break;
+            default:
+                ay="YANLIŞ BİLGİ";
+                break;
+        }
+        txtview.setText(ay);
     }
 }
